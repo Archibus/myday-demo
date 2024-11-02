ea5d1155d5bf1d40bc57eb6e7b613a7d316e553d
Jeff Martin <jeff.martin@spaceiq.com>
2024-11-02 09:40:10 -0400

    initial commit:

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import styled from 'styled-components';
+import {SwiftConnect} from "@archibus/swift-connect";
+import {Preferences} from "@capacitor/preferences";
+
+const Container = styled.div`
+    border: 4px solid #61dafb;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Button = styled.button`
+    height: 40px;
+    width: 120px;
+    color: white;
+    background-color: #61dafb;
+`;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        SwiftConnect.addListener('WalletDataReady', (data) => {
+            console.log('onLogin', data);
+            alert('WalletDataReady ' + JSON.stringify(data));
+        });
+    }
+    , []);
+    const onButtonClick = async () => {
+        await SwiftConnect.loginWithEmail({username: "jeff.martin@eptura.com", password: "Jo&9hUS2"});
+    }
+
+    const onTokenButtonClick = async () => {
+        const {value} = await Preferences.get({key: 'idToken'});
+        alert(value);
+    }
+
+    return (
+        <Container>
+            <Button onClick={onButtonClick}>Click me</Button>
+            <Button onClick={onTokenButtonClick}>Get Token</Button>
+        </Container>
+    );
 }
 
 export default App;
