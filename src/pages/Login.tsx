471bcf3520fd0185db3f9bbcbda33131733f272b
Jeff Martin <jeff.martin@spaceiq.com>
2024-11-07 17:38:46 -0500

    wallet data test

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -62,6 +62,30 @@ export const Login = () => {
             alert('Login failed');
         }
     }
+
+    const onLoginUser = async () => {
+        try {
+            setIsLoading(true);
+            await SwiftConnect.loginWithUser()
+            setIsLoading(false);
+        } catch(e) {
+            setIsLoading(false);
+            alert('Login failed');
+        }
+    }
+
+    const onFetchWalletData = async () => {
+        try {
+            setIsLoading(true);
+            const walletData = await SwiftConnect.fetchWalletData();
+            alert(JSON.stringify(walletData));
+            setIsLoading(false);
+        } catch(e) {
+            setIsLoading(false);
+            alert('Fetch Wallet Data failed');
+        }
+    }
+
     return (
         <>
             {isLoading ? <Loader isLoading={isLoading}/>: null}
@@ -70,6 +94,10 @@ export const Login = () => {
                     <Input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
                     <Input type="password" autoComplete="new-password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                     <Button onClick={onLoginButtonClick}>Login</Button>
+
+                    <Button onClick={onLoginUser}>Login User</Button>
+                    <Button onClick={onFetchWalletData}>Fetch Wallet Data</Button>
+
                 </LoginContainer>
 
             </Container>
